f20fafd6bf6be2638e41647b3b247c34f9548a47
FHegardt <39218412+FHegardt@users.noreply.github.com>
2022-03-24 11:52:50 +0100

    deployment done and working!

--- Labb/production_model.py
+++ Labb/production_model.py
@@ -1,5 +1,12 @@
 import pandas as pd
 import joblib
+from sklearn.preprocessing import StandardScaler
 
 
 myModel = joblib.load("./Labb./KNN_model.pkl")
+test_data = pd.read_csv("./Labb./test_samples.csv", index_col=0)
+
+probability = myModel.predict_proba(test_data)
+prediction = myModel.predict(test_data)
+predicted_data = pd.DataFrame(probability,prediction)
+predicted_data.to_csv("./Labb/predictions.csv",  index_label= ["Prediction", "Proba 0", "Proba 1"])
