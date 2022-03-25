7c79e331550a6d79de3e74fa622d036fbf792676
FHegardt <39218412+FHegardt@users.noreply.github.com>
2022-03-25 09:54:58 +0100

    added comments and updated models hyperParams

--- Labb/production_model.py
+++ Labb/production_model.py
@@ -3,10 +3,11 @@ import joblib
 from sklearn.preprocessing import StandardScaler
 
 
-myModel = joblib.load("./Labb./best_model.pkl")
-test_data = pd.read_csv("./Labb./test_samples.csv", index_col=0)
+myModel = joblib.load("./Labb./best_model.pkl") #loading my model
+test_data = pd.read_csv("./Labb./test_samples.csv", index_col=0) #loading test-data
 
-probability = myModel.predict_proba(test_data)
-prediction = myModel.predict(test_data)
+probability = myModel.predict_proba(test_data) #predicting probability on test data
+prediction = myModel.predict(test_data) #predicting on test data
 predicted_data = pd.DataFrame(probability,prediction)
-predicted_data.to_csv("./Labb/predictions.csv",  index_label= ["Prediction", "Probability class 0", "Probability class 1"])
+predicted_data.to_csv("./Labb/predictions.csv",  
+index_label= ["Prediction", "Probability class 0", "Probability class 1"]) #saving the results with titles.
